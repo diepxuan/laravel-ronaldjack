8894a357008dc6efa0efeeda6b81032d4f1e09f1
diepxuan <ductn@diepxuan.com>
2024-07-08 13:52:41 +0000

    add libs and controller

--- src/Models/Machine.php
+++ src/Models/Machine.php
@@ -8,9 +8,21 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2024-07-05 15:35:21
+ * @lastupdate 2024-07-07 21:14:55
  */
 
 namespace Diepxuan\RonaldJack\Models;
 
-class Machine {}
+use Diepxuan\Core\Models\Package;
+
+class Machine
+{
+    private $_zkemkeeper;
+
+    public function __construct()
+    {
+        $_zkemkeeper       = Package::path('diepxuan/laravel-core', 'libs/zkemkeeper.dll');
+        $this->_zkemkeeper = new \COM($_zkemkeeper) || new \Exception();
+        dd($this->_zkemkeeper);
+    }
+}
