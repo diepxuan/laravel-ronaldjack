46f16f1bfbfdea1e175929da4978fc090e53a966
diepxuan <ductn@diepxuan.com>
2024-12-12 15:51:31 +0000

    create app layout

---
+++ config/config.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2024-12-12 22:48:07
+ */
+
+use Diepxuan\RonaldJack;
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2024-12-12 22:15:20
+ */
+
+return [
+    'name'      => 'diepxuan/laravel-ronaldjack',
+    'namespace' => RonaldJack::class,
+];
